3cda994f19c81e15ca2858c53129ea92ce4f148d
Rohit Garg <rohit.garg@meesho.com>
2022-07-31 11:27:32 +0530

    /api/students/index

--- src/main/java/com/Meesho/rest/StudentRestController.java
+++ src/main/java/com/Meesho/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.Meesho.rest;
 
 import com.Meesho.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,16 +17,23 @@ public class StudentRestController {
 
     List<Student> studentList;
 
-    //define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    @PostConstruct
+    public void loadData(){
         studentList = new ArrayList<Student>();
 
         studentList.add(new Student("Rohit", "Garg"));
         studentList.add(new Student("Harshil", "Jain"));
         studentList.add(new Student("Vatsal", "Agarwal"));
+    }
+    //define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
 
         return studentList;
+    }
 
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id){
+        return studentList.get(id);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,15 @@
 <html>
 <body>
 
-Spring REST DEMO
+<h1>Spring REST Demo </h1>
 
+<p>
 <a href = "/rest-with-spring/test/hello">Hello World</a>
+</p>
+
+<p>
+<a href = "/rest-with-spring/api/students">Get all students</a>
+</p>
 
 </body>
 </html>
